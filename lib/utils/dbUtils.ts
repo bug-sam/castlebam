1c702623b645b80fd7d84345108968bd97644421
bug-sam <samuel.tate@outlook.com>
2025-01-28 15:36:04 -0500

    map out getting data from the db

--- lib/utils/dbUtils.ts
+++ lib/utils/dbUtils.ts
@@ -2,7 +2,9 @@ import client from "@/lib/db/client";
 
 const getDB = () => process.env.NODE_ENV === "development" ? "dev" : "prod";
 
-export const insert = async (table: string, row: any) => {
+export type Table = "users" | "shows" | "venues";
+
+export const insert = async (table: Table, row: any) => {
     try {
         await client.connect();
         await client.db(getDB()).collection(table).insertOne(row);
@@ -11,4 +13,22 @@ export const insert = async (table: string, row: any) => {
     } finally {
         await client.close();
     }
+}
+
+export const getCollection = async <T>(table: Table): Promise<T[]> => {
+    let results: T[] = [];
+    try {
+        await client.connect();
+        results = await client.db(getDB()).collection(table).find().toArray() as T[];
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+    return results;
+}
+
+export const getRow = async <T>(table: Table, predicate: (row: T) => boolean): Promise<T> => {
+    const collection = await getCollection<T>(table);
+    return collection.find(predicate) as T;
 }
