b29e928ee8b7714b474610d9d1acfedadc70852f
Sam Tate <samuel.tate@outlook.com>
2025-04-22 15:51:00 -0400

    super base level admin page so that dom can make it look nice

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -9,3 +9,11 @@ export const signup = async (formData: FormData) => {
         type: "regular",
     });
 }
+
+export const signin = async (formData: FormData) => {
+    addUser({
+        username: formData.get("username") as string,
+        password: formData.get("password") as string,
+        type: "regular",
+    });
+}
