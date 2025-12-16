aab0add84166d28367dd3a814c70a862f00fc181
Sam Tate <samuel.tate@outlook.com>
2025-12-16 17:28:22 -0500

    fetch venue data from server using swr

--- app/venues/page.tsx
+++ app/venues/page.tsx
@@ -5,57 +5,17 @@
 // admins will have access to the list of submissions and they can accept them if they feel that they should be added to the site
 
 import { SubmitVenueForm } from "@/components/forms/VenueForm";
-import { useEffect, useState } from "react";
-
-const fetchVenues = async () => {
-    const response = await fetch("/api/venues");
-    return await response.json();
-};
+import { useState } from "react";
+import useSWR from "swr";
 
 const Venues = () => {
-    // Replace API call with dummy venue data
-    const venues = [
-        {
-            id: 1,
-            name: "Ortliebs",
-            location: "Philly",
-            instagram: "@ortliebsbar",
-            isAllAges: false,
-            isDefunct: false,
-            venueType: "bar",
-            description: "Twin Peaks-esq bar in No Libs",
-        },
-        {
-            id: 2,
-            name: "Silver Age Hq",
-            location: "philly",
-            instagram: "@silverAgeHq",
-            isAllAges: true,
-            isDefunct: false,
-            venueType: "basement",
-            description: "disney",
-        },
-        {
-            id: 3,
-            name: "lovely place",
-            location: "philly",
-            instagram: "@castlebam",
-            isAllAges: true,
-            isDefunct: true,
-            venueType: "house",
-            description: "Used to be incredible. RIP 2018-2023.",
-        },
-    ];
+    const { data, error, isLoading } = useSWR("/api/venues", (url: string) => fetch(url).then((res) => res.json()));
 
     const [openVenueId, setOpenVenueId] = useState<number | null>(null);
 
     const toggleVenue = (id: number) => {
         setOpenVenueId((prev) => (prev === id ? null : id));
     };
-
-    useEffect(() => {
-        console.log(venues);
-    }, [venues]);
 
     return (
         <div className="p-4 flex gap-12">
@@ -65,48 +25,52 @@ const Venues = () => {
 
             {/* Rocks */}
             <div className="w-1/2 grid grid-cols-2 gap-6">
-                {venues?.map((venue: any) => (
-                    <div key={venue.id} className="flex flex-col items-center">
-                        {/* Rock Image Button */}
-                        <button
-                            className="rock-button"
-                            onClick={() => toggleVenue(venue.id)}
-                        >
-                            <img src="rock.png" />
-                        </button>
+                {isLoading ? (
+                    <>loading...</>
+                ) : (
+                    data.map((venue: any) => (
+                        <div key={venue._id} className="flex flex-col items-center">
+                            {/* Rock Image Button */}
+                            <button
+                                className="rock-button"
+                                onClick={() => toggleVenue(venue._id)}
+                            >
+                                <img src="rock.png" />
+                            </button>
 
-                        {/* Venue Details */}
-                        {openVenueId === venue.id && (
-                            <div className="venue-details">
-                                <p>
-                                    <strong>Name:</strong> {venue.name}
-                                </p>
-                                <p>
-                                    <strong>Location:</strong> {venue.location}
-                                </p>
-                                <p>
-                                    <strong>Instagram:</strong>{" "}
-                                    {venue.instagram}
-                                </p>
-                                <p>
-                                    <strong>Type:</strong> {venue.venueType}
-                                </p>
-                                <p>
-                                    <strong>All Ages:</strong>{" "}
-                                    {venue.isAllAges ? "Yes" : "No"}
-                                </p>
-                                <p>
-                                    <strong>Defunct:</strong>{" "}
-                                    {venue.isDefunct ? "Yes" : "No"}
-                                </p>
-                                <p>
-                                    <strong>Description:</strong>{" "}
-                                    {venue.description}
-                                </p>
-                            </div>
-                        )}
-                    </div>
-                ))}
+                            {/* Venue Details */}
+                            {openVenueId === venue._id && (
+                                <div className="venue-details">
+                                    <p>
+                                        <strong>Name:</strong> {venue.name}
+                                    </p>
+                                    <p>
+                                        <strong>Location:</strong> {venue.location}
+                                    </p>
+                                    <p>
+                                        <strong>Instagram:</strong>{" "}
+                                        {venue.instagram}
+                                    </p>
+                                    <p>
+                                        <strong>Type:</strong> {venue.venueType}
+                                    </p>
+                                    <p>
+                                        <strong>All Ages:</strong>{" "}
+                                        {venue.isAllAges ? "Yes" : "No"}
+                                    </p>
+                                    <p>
+                                        <strong>Defunct:</strong>{" "}
+                                        {venue.isDefunct ? "Yes" : "No"}
+                                    </p>
+                                    <p>
+                                        <strong>Description:</strong>{" "}
+                                        {venue.description}
+                                    </p>
+                                </div>
+                            )}
+                        </div>
+                    ))
+                )}
             </div>
         </div>
     );
