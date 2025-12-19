b19e429657a33a2a634985777e8bf6b27294fbee
Sam Tate <samuel.tate@outlook.com>
2025-12-18 23:44:08 -0500

    add useClickAway hook

---
+++ lib/hooks/useClickAway.ts
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+const useClickAway = <T extends HTMLElement>(ref: React.RefObject<T>, cb: () => void) => {
+    useEffect(() => {
+        const listener = (e: MouseEvent) => {
+            if (!ref?.current.contains(e.target as Node)) {
+                cb();
+            }
+        };
+        window.addEventListener('click', listener);
+        return window.removeEventListener.bind(null, 'click', listener as EventListener);
+    }, []);
+};
+
+export default useClickAway;
